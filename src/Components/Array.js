11a1c37392b3db93c99f84a6a6ffbf5c8528c7ac
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-05 14:02:05 -0500

    Update Array.js

--- src/Components/Array.js
+++ src/Components/Array.js
@@ -2,6 +2,14 @@ function Array(){
     const items = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
     return(
         <>
+         <div>
+      <h3>Fruit List</h3>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
         </>
     );
 }
